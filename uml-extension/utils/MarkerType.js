6aef8a7fb5a4e1141b4f14b955a4d591ef8a4027
dfuchss <develop@fuchss.org>
2021-10-21 12:52:03 +0000

    Deploying to gh-pages from @ LiSSA-Approach/uml-image-annotator@d8d66e8bcf63ac645a0992aca05936b142bd2dca 🚀

--- uml-extension/utils/MarkerType.js
+++
@@ -1,9 +0,0 @@
-/**
- * Available marker types for UML connections
- * 
- * @module MarkerType
- */
- export default Object.freeze({
-    START: "start",
-    END: "end"
-})
--- uml-extension/utils/Mode.js
+++
@@ -1,9 +0,0 @@
-/**
- * Modes for Annotator tool. Decides annotated diagram type
- * 
- * @module Mode
- */
- export default Object.freeze({
-    BPMN: "BPMN",
-    UML_CLASS: "UML Class"
-})
